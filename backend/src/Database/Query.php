167d18eec0ac67e2914074c961509bd74e10228e
Ahmed Gawish <amgawish@student.aast.edu>
2025-04-12 19:10:47 +0200

    removed dead code

--- backend/src/Database/Query.php
+++ backend/src/Database/Query.php
@@ -8,108 +8,6 @@ use PDO, PDOException;
 
 class Query extends Database
 {
-    public function getCategories()
-    {
-        try {
-            $connection = $this->getConnection();
-            $sql = "SELECT id, name FROM categories;";
-            $stmt = $connection->query($sql);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "Error:" . $e->getMessage();
-        }
-    }
-
-    public function getAllProducts()
-    {
-        try {
-            $connection = $this->getConnection();
-            $sql = "
-                SELECT 
-                    p.id AS product_id,
-                    p.name,
-                    p.in_stock,
-                    p.description,
-                    p.category,
-                    p.brand,
-
-                    pr.id AS price_id,
-                    pr.amount,
-                    pr.currency_label,
-                    pr.currency_symbol,
-
-                    g.id AS gallery_id,
-                    g.image_url,
-
-                    av.id AS attribute_value_id,
-                    av.value,
-                    av.display_value,
-
-                    a.id AS attribute_id,
-                    a.name AS attribute_name,
-                    a.type AS attribute_type
-
-                FROM products p
-                LEFT JOIN prices pr ON pr.product_id = p.id
-                LEFT JOIN galleries g ON g.product_id = p.id
-                LEFT JOIN product_attribute_values pav ON pav.product_id = p.id
-                LEFT JOIN attribute_values av ON pav.attribute_value_id = av.id
-                LEFT JOIN attributes a ON av.attribute_id = a.id
-                ORDER BY p.id;
-            ";
-            $stmt = $connection->query($sql);
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-    }
-
-    public function getProductByID(string $productId)
-    {
-        try {
-            $connection = $this->getConnection();
-            $sql = "
-                SELECT 
-                    p.id AS product_id,
-                    p.name,
-                    p.in_stock,
-                    p.description,
-                    p.category,
-                    p.brand,
-
-                    pr.id AS price_id,
-                    pr.amount,
-                    pr.currency_label,
-                    pr.currency_symbol,
-
-                    g.id AS gallery_id,
-                    g.image_url,
-
-                    av.id AS attribute_value_id,
-                    av.value,
-                    av.display_value,
-
-                    a.id AS attribute_id,
-                    a.name AS attribute_name,
-                    a.type AS attribute_type
-
-                FROM products p
-                LEFT JOIN prices pr ON pr.product_id = p.id
-                LEFT JOIN galleries g ON g.product_id = p.id
-                LEFT JOIN product_attribute_values pav ON pav.product_id = p.id
-                LEFT JOIN attribute_values av ON pav.attribute_value_id = av.id
-                LEFT JOIN attributes a ON av.attribute_id = a.id
-                WHERE p.id = :productId;
-            ";
-            $stmt = $connection->prepare($sql);
-            $stmt->bindParam(':productId', $productId, PDO::PARAM_STR);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-    }
-
     public function placeOrder(array $items): bool
     {
         try {
@@ -153,6 +51,7 @@ class Query extends Database
 
             $connection->commit();
             return true;
+            
         } catch (PDOException $e) {
             if (!empty($connection)) {
                 $connection->rollBack();
