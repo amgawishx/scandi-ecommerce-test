73f875bed5ec225f88ab91ae4cfd94286e6471c4
Ahmed Gawish <amgawish@student.aast.edu>
2025-04-16 12:26:31 +0200

    updated hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     allowedHosts: [
       "all",
       "84b5e91d-ed35-4cbe-8eac-9578d902fb29-00-2z21rqvxwzpfa.spock.replit.dev",
+      "static.9.49.235.167.clients.your-server.de",
     ],
   },
   plugins: [react()],
