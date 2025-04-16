90899244d098d6e0f8f6c71359b4ac1460fa1008
Ahmed Gawish <amgawish@student.aast.edu>
2025-04-16 12:13:50 +0200

    changed frontend port to 80

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
+    port: "80",
     allowedHosts: [
       "all",
       "84b5e91d-ed35-4cbe-8eac-9578d902fb29-00-2z21rqvxwzpfa.spock.replit.dev",
